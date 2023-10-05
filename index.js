264bb6d2797b5be483440c5862041754969e7734
Derek Mercedes <mercedes.derek@gmail.com>
2023-10-05 14:36:54 -0600

    resubmits after confirming functionality

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
+const button = document.getElementById('button')
+
 function addingEventListener() {
+
+    button.addEventListener('click', clicked)
+
 }
+
+function clicked(){
+    alert('You clicked me!')
+}
+
+addingEventListener();
